6f5fac91ea4c30353e7cfcfd214c4d59d70136b6
Brian Marick <marick@exampler.com>
2009-06-02 16:21:36 -0500

    Added gear page

--- controller/main.rb
+++ controller/main.rb
@@ -6,10 +6,4 @@
 # this will force the controller to be mounted on: /otherurl
 
 class MainController < Controller
-  # the string returned at the end of the function is used as the html body
-  # if there is no template for the action. if there is a template, the string
-  # is silently ignored
-  def notemplate
-    "there is no 'notemplate.xhtml' associated with this action"
-  end
 end
