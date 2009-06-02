019b75ec961fa2b90bc3fbdb1443a49119329cce
Brian Marick <marick@exampler.com>
2009-06-02 15:49:13 -0500

    pulled index.html header out into default layout

--- controller/main.rb
+++ controller/main.rb
@@ -6,11 +6,6 @@
 # this will force the controller to be mounted on: /otherurl
 
 class MainController < Controller
-  # the index action is called automatically when no other action is specified
-  def index
-    @title = "Welcome to Ramaze!"
-  end
-
   # the string returned at the end of the function is used as the html body
   # if there is no template for the action. if there is a template, the string
   # is silently ignored
