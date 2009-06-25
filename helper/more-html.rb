d4c3a7332366560d692b7e8d360592120a11b7b0
Brian Marick <marick@exampler.com>
2009-06-25 17:38:06 -0500

    Display footer only on home page.

--- helper/more-html.rb
+++ helper/more-html.rb
@@ -4,17 +4,17 @@ module Erector
 
   class Widget
 
-  def self.combine(result, tag1, tag2)
-    defn = "def #{result}(*args, &block)
+    def self.combine(result, tag1, tag2)
+      defn = "def #{result}(*args, &block)
               #{tag1} { #{tag2}(*args, &block) }
             end"
-    module_eval defn
-  end
+      module_eval defn
+    end
 
-  combine :lip, :li, :p
-  combine :pem, :p, :em
-  combine :pb, :p, :b
+    combine :lip, :li, :p
+    combine :pem, :p, :em
+    combine :pb, :p, :b
+
+  end
 
 end
-
-  end
