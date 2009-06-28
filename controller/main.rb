7df975e56471a5563a64d004555dd4afeb1c0734
Brian Marick <marick@exampler.com>
2009-06-28 13:47:58 -0500

    Extract method

--- controller/main.rb
+++ controller/main.rb
@@ -1,15 +1,22 @@
 require 'helper/widgets'
 
 class MainController < Ramaze::Controller
-  def index
-    require 'view/index-page'
-    maker = WithinSiteLinkMaker.new(:current_route => :index)
-    IndexPage.new(:within_site_link_maker => maker).to_pretty
+  def self.has_page(name)
+    name = name.to_s
+    page_klass = name.capitalize + 'Page'
+
+    defn = %Q{
+      def #{name}
+        require "view/#{name}-page"
+        maker = WithinSiteLinkMaker.new(:current_route => :#{name})
+        #{page_klass}.new(:within_site_link_maker => maker).to_pretty
+      end
+    }
+    puts defn
+    class_eval defn
   end
 
-  def gear
-    require 'view/gear-page'
-    maker = WithinSiteLinkMaker.new(:current_route => :gear)
-    GearPage.new(:within_site_link_maker => maker).to_pretty
-  end
+  has_page :index
+  has_page :gear
+
 end
