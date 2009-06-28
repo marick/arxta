8b46111094cf88ef3d6cbd275452d26655600867
Brian Marick <marick@exampler.com>
2009-06-28 12:24:43 -0500

    Use WithinSiteLinkMaker to make sidebar.

--- controller/main.rb
+++ controller/main.rb
@@ -1,11 +1,15 @@
+require 'helper/widgets'
+
 class MainController < Ramaze::Controller
   def index
     require 'view/index-page'
-    IndexPage.new.to_pretty
+    maker = WithinSiteLinkMaker.new(:current_route => :index)
+    IndexPage.new(:within_site_link_maker => maker).to_pretty
   end
 
   def gear
     require 'view/gear-page'
-    GearPage.new.to_pretty
+    maker = WithinSiteLinkMaker.new(:current_route => :gear)
+    GearPage.new(:within_site_link_maker => maker).to_pretty
   end
 end
